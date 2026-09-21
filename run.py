cbe91134be4c5fa1d9223deacb5ea913213ef052
kangwasiame <kangwasiame4@gmail.com>
2026-09-21 16:29:11 +0200

    Improve local app startup typing

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from typing import cast
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host=app.config['HOST'], port=app.config['PORT'])
+    app.run(
+        debug=True,
+        host=cast(str, app.config['HOST']),
+        port=cast(int, app.config['PORT']),
+    )
